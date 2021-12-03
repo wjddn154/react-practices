d9e4b5fa81a4fc1c7b969749510ba62273295b85
wjddn154 <gjwjddn154@gmail.com>
2021-12-03 11:27:27 +0900

    guestbook #2

--- guestbook/src/MessageList.js
+++ guestbook/src/MessageList.js
@@ -11,13 +11,24 @@ export default function MessageList({messages}) {
     const refForm = useRef(null);
     const [isOpen, setIsOpen] = useState(false);
 
+    const handleSubmit = (e) => {
+        e.preventDefault();
+
+        console.log("삭제!!!!!");
+    }
+    const notifyDeleteMessage = (no) => {
+        console.log('delete!!! : ' + no);
+        setIsOpen(true);
+    }
+
     return (
         <Fragment>
             <ul className={styles.MessageList}>
                 {messages.map(message => <Message key={`guestbook_message_${message.no}`}
                                                   no={message.no}
                                                   name={message.name}
-                                                  message={message.message} />)}
+                                                  message={message.message}
+                                                  notifyDeleteMessage={ notifyDeleteMessage } />)}
             </ul>
             <Modal
                 isOpen={isOpen}
@@ -28,7 +39,10 @@ export default function MessageList({messages}) {
                 style={{content: {width: 350}}}>
                 <h1>비밀번호입력</h1>
                 <div>
-                    <form className={styles.DeleteForm}>
+                    <form 
+                        ref={refForm}
+                        className={styles.DeleteForm}
+                        onSubmit={handleSubmit}>
                         <label>작성시 입력했던 비밀번호를 입력 하세요.</label>
                         <input
                             type={'password'}
@@ -38,7 +52,10 @@ export default function MessageList({messages}) {
                     </form>
                 </div>
                 <div className={modalStyles['modal-dialog-buttons']}>
-                    <button>확인</button>
+                    <button onClick={ () => {
+                                        //트리거 이벤트
+                        refForm.current.dispatchEvent(new Event("submit", {cancelable: true, bubbles: true}))
+                    }}>확인</button>
                     <button onClick={() => setIsOpen(false)}>취소</button>
                 </div>
             </Modal>
